eceb416b2f39257da142f5310abd392e37c8dc9c
Natan Facchin <maxmousee@gmail.com>
2017-01-06 00:28:47 -0200

    Added 4sq lib, add basic query, added icons

--- iOS/NearRestaurantVoter/NearRestaurantVoter/ViewController.swift
+++ iOS/NearRestaurantVoter/NearRestaurantVoter/ViewController.swift
@@ -8,38 +8,105 @@
 
 import UIKit
 import MapKit
+import QuadratTouch
+import CoreLocation
 
-class ViewController: UIViewController {
-
-    @IBOutlet weak var aMap: MKMapView!
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager = CLLocationManager();
+    var location: CLLocation!;
+    var locationStatus = "Not Started";
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+        //print("locations = \(locValue.latitude) \(locValue.longitude)")
+        location = manager.location;
+    }
+    
+    func getUserLocation() {
+        // Ask for Authorisation from the User.
+        self.locationManager.requestAlwaysAuthorization()
+        
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.requestAlwaysAuthorization()
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func setupFoursquareSession() -> Session {
+        let client = Client(clientID:       "2OMA3WBEWYITHKPHSHUWRX43PTXRLEEJKJZZKVWZ0AU2EWEM",
+                            clientSecret:   "30SIYXA43ZN14UQ3XTVFVFV1S50YNSM4VBSHX4DHEWG5YKKM",
+                            redirectURL:    "NearRestaurantVoter://foursquare")
+        let configuration = Configuration(client:client)
+        Session.setupSharedSessionWithConfiguration(configuration)
+        
+        return Session.sharedSession();
+    }
+    
+    func searchNearbyRestaurants() {
+        let foursquareSession = setupFoursquareSession();
+        var parameters = [Parameter.query:"Restaurants"]
+        //let locationDict = ["ll": String(format:"%.8f,%.8f", self.location.coordinate.latitude,self.location.coordinate.longitude)]
+        let locationDict = ["ll": String(format:"%.2f,%.2f", -30.06,-51.16)]
+        parameters += locationDict
+        print(parameters);
+        let searchTask = foursquareSession.venues.search(parameters) {
+            (result) -> Void in
+            if let response = result.response {
+                print(result)
+                print(response)
+                print(response["venues"] ?? "none")
+                //self.venues = response["venues"] as [JSONParameters]?
+                //self.tableView.reloadData()
+            }
+        }
+        searchTask.start()
+    }
+    
+    // authorization status
+    private func locationManager(manager: CLLocationManager!,
+                         didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        var shouldIAllow = false
+        
+        switch status {
+        case CLAuthorizationStatus.restricted:
+            locationStatus = "Restricted Access to location"
+        case CLAuthorizationStatus.denied:
+            locationStatus = "User denied access to location"
+        case CLAuthorizationStatus.notDetermined:
+            locationStatus = "Status not determined"
+        default:
+            locationStatus = "Allowed to location Access"
+            shouldIAllow = true
+        }
+        
+        if (shouldIAllow == true) {
+            NSLog("Location to Allowed")
+            // Start location services
+            locationManager.startUpdatingLocation()
+        } else {
+            NSLog("Denied access: \(locationStatus)")
+        }
+    }
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        self.aMap.setUserTrackingMode(MKUserTrackingMode.followWithHeading, animated: true);
+        super.viewDidLoad();
+        getUserLocation();
+        searchNearbyRestaurants();
         
-        // Create and initialize a search request object.
-        let request = MKLocalSearchRequest();
-        request.naturalLanguageQuery = "restaurant";
-        request.region = self.aMap.region;
-        
-        // Create and initialize a search object.
-        let search = MKLocalSearch(request: request);
-        
-        
-        // Start the search and display the results as annotations on the map.
-        search.start(completionHandler: {response,error in
-            var placemarks = [MKAnnotation]();
-            for item in (response?.mapItems)! {
-                placemarks.append(item.placemark);
-            }
-            self.aMap.removeAnnotations(self.aMap.annotations);
-            self.aMap.showAnnotations(placemarks, animated: false);
-            })
-        
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated);
     }
 
     override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+        super.didReceiveMemoryWarning();
         // Dispose of any resources that can be recreated.
     }
 
